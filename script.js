1a68a29ea7570dd2be04262abbed954584c9882e
Chayan Chandan das <chayanchandandas@gmail.com>
2022-10-23 20:49:38 +0600

    add js complite file

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btnEl = document.querySelector('.btn');
-
+const copyEl = document.querySelector('.far');
+const inputEl = document.getElementById('input');
+const alartEl = document.querySelector('.alert-container')
 btnEl.addEventListener('click', () => {
     const passwordChars = '0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     const passLangth = 14;
@@ -10,5 +12,22 @@ btnEl.addEventListener('click', () => {
         console.log(randomNum);
         generatePass += passwordChars.substring(randomNum, randomNum + 1)
     };
-    console.log(generatePass)
-})
+    inputEl.value = generatePass;
+    alartEl.innerText = generatePass + ' Copide';
+
+});
+
+copyEl.addEventListener("click", () => {
+    copyPassword();
+});
+
+function copyPassword() {
+    inputEl.select();
+    inputEl.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(inputEl.value);
+    alartEl.classList.remove('active');
+
+    setTimeout(() => {
+        alartEl.classList.add('active')
+    }, 2000);
+}
